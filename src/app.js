92388605657ab8b4e9e51c3ef8053dd61bec321e
leonardo munoz 🌑🌍 <168220131+leonardomunooz@users.noreply.github.com>
2024-06-14 05:38:37 +0000

    se ha modificado para que el html y css se renderizen a traves de javascript

--- src/app.js
+++ src/app.js
@@ -6,6 +6,36 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let body = document.querySelector("body");
+  let card = document.querySelector("#card");
+  let flexCard = document.createElement("div");
+  let cTop = document.createElement("div");
+  let iconTop = document.createElement("i");
+  let number = Math.floor(Math.random() * 5);
+  let p = document.createElement("p");
+  let cBottom = document.createElement("div");
+  let iconBottom = document.createElement("i");
+
+  console.log(body);
+  body.style.backgroundColor = "green";
+  body.style.display = "flex";
+  body.style.height = "100%";
+  body.style.justifyContent = "center";
+  body.style.alignItems = "center";
+
+  card.setAttribute("class", "card-content"); // utilizanado setAttribute
+  card.append(flexCard);
+
+  flexCard.classList.add("flexCard"); // utilizando classlist
+  flexCard.append(cTop);
+  flexCard.append(p);
+  flexCard.append(cBottom);
+
+  cTop.append(iconTop);
+  iconTop.setAttribute("class", "fas fa-heart");
+  cBottom.append(iconBottom);
+  p.setAttribute("class", "numero-carta");
+  p.innerHTML = number;
+  iconBottom.setAttribute("class", "fas fa-heart");
+  cBottom.style.alignSelf = "flex-end";
 };
