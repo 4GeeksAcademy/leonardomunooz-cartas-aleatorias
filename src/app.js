a2ecd7016243ff9ab47833704915895e4b7c0a9e
leonardo munoz 🌑🌍 <168220131+leonardomunooz@users.noreply.github.com>
2024-06-18 19:21:42 +0000

    se ha icorporado los valores de los palos y las cartas en arrays

--- src/app.js
+++ src/app.js
@@ -10,31 +10,52 @@ window.onload = function() {
   let flexCard = document.createElement("div");
   let cTop = document.createElement("div");
   let iconTop = document.createElement("i");
-  let number = Math.ceil(Math.random() * 4);
+
   let p = document.createElement("p");
   let cBottom = document.createElement("div");
   let iconBottom = document.createElement("i");
 
   console.log(body);
+
+  // estilos del boby y centrado de la carta
   body.style.backgroundColor = "green";
   body.style.display = "flex";
   body.style.height = "100%";
   body.style.justifyContent = "center";
   body.style.alignItems = "center";
 
+  //  agrega estilos de carta y la agrega al body
   card.setAttribute("class", "card-content"); // utilizanado setAttribute
   card.append(flexCard);
 
+  //  agregando  elementos y posic
   flexCard.classList.add("flexCard"); // utilizando classlist
   flexCard.append(cTop);
   flexCard.append(p);
   flexCard.append(cBottom);
 
+  let palos = ["♦", "♥", "♠", "♣"];
+  let palosNumber = Math.floor(Math.random() * palos.length);
+  let cartas = ["A", 2, "3", 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+  let numeroCarta = Math.floor(Math.random() * cartas.length);
+  function getPalo(array, randomNumber) {
+    let numeroRandom = undefined;
+    for (let index = 0; index < array.length - 1; index++) {
+      numeroRandom = array[randomNumber];
+    }
+    return numeroRandom;
+  }
+  let palo = getPalo(palos, palosNumber);
+  let carta = getPalo(cartas, numeroCarta);
+
+  console.log(carta);
   cTop.append(iconTop);
-  iconTop.setAttribute("class", "fas fa-heart");
+  iconTop.textContent = palo;
+  iconTop.setAttribute("class", "fas");
   cBottom.append(iconBottom);
-  p.setAttribute("class", "numero-carta");
-  p.innerHTML = number;
-  iconBottom.setAttribute("class", "fas fa-heart");
   cBottom.style.alignSelf = "flex-end";
+  iconBottom.textContent = palo;
+  iconBottom.setAttribute("class", "fas");
+  p.setAttribute("class", "numero-carta");
+  p.innerHTML = carta;
 };
